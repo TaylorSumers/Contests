ba1587ae2bf362fa1fd222032251e54f6c5eb73b
TaylorSumers <suvorint@gmail.com>
2026-01-06 22:53:07 +0300

    Task J c2

--- main.cpp
+++ main.cpp
@@ -5,13 +5,17 @@
 #include <set>
 
 struct Sheep {
-  size_t id;
+  uint id;
   uint a;
 };
 
 struct Dog {
-  size_t id;
+  uint id;
   uint b;
+
+  bool operator<(const Dog& other) const {
+    return id < other.id;
+  }
 };
 
 size_t m, n;
@@ -27,16 +31,20 @@ void Input() {
   std::cin >> m >> n;
   sheeps.reserve(m);
 
-  for (size_t i = 0; i < m; i++) {
+  for (uint i = 0; i < m; i++) {
     Sheep sheep{i};
     std::cin >> sheep.a;
     sheeps.push_back(sheep);
   }
-  for (size_t i = 0; i < n; i++) {
+  for (uint i = 0; i < n; i++) {
     Dog dog{i};
     std::cin >> dog.b;
     dogs.insert(dog);
   }
+}
+
+bool FindDogs(uint a, uint& b_j, uint& b_k) {
+
 }
 
 int main() {
@@ -44,4 +52,18 @@ int main() {
   Input();
 
   std::sort(sheeps.begin(), sheeps.end(), [](const Sheep& l_sheep, const Sheep& r_sheep) { return l_sheep.a < r_sheep.a; });
+  uint count = 0;
+  std::vector<std::vector<uint>> res;
+  for (Sheep sheep : sheeps) {
+    uint b_j, b_k;
+    if (FindDogs(sheep.a, b_j, b_k)) {
+      count++;
+      res.push_back(std::vector{sheep.id, b_j, b_k});
+    }
+  }
+
+  std::cout << count << '\n';
+  for (std::vector el : res) {
+    std::cout << el[0] << ' ' << el[1] << ' ' << el[2] << '\n';
+  }
 }
