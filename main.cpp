836ffbcfe64667e25c445cfc16cd3000f388f86b
TaylorSumers <suvorint@gmail.com>
2026-01-06 19:48:36 +0300

    Task I c1

--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
 #include <algorithm>
-#include <cstdint>
 #include <iostream>
 #include <vector>
+#include <cstdint>
 
-uint n;
-std::vector<int> a;
+struct Test {
+  size_t id;
+  uint a;
+  uint b;
+};
+
+size_t n;
+std::vector<Test> tests;
 
 void SetFastIo() {
   std::ios::sync_with_stdio(false);
@@ -13,54 +19,53 @@ void SetFastIo() {
 
 void Input() {
   std::cin >> n;
-  a.reserve(n);
-  int ai;
-  for (int i = 0; i < n; i++) {
-    std::cin >> ai;
-    a.push_back(ai);
+  tests.resize(n);
+  uint temp;
+  for (size_t i = 0; i < n; i++) {
+    std::cin >> temp;
+    tests[i].id = i;
+    tests[i].a = temp;
   }
-}
-
-std::vector<int> Merge(const std::vector<int>& l,
-                       const std::vector<int>& r) {
-  std::vector<int> res;
-  size_t l_i = 0, r_i = 0;
-  while (l_i < l.size() && r_i < r.size()) {
-    if (l[l_i] > r[r_i]) {
-      res.push_back(r[r_i]);
-      r_i++;
-    } else {
-      res.push_back(l[l_i]);
-      l_i++;
-    }
+  for (size_t i = 0; i < n; i++) {
+    std::cin >> temp;
+    tests[i].b = temp;
   }
-
-  while (l_i < l.size()) {
-    res.push_back(l[l_i]);
-    l_i++;
-  }
-  while (r_i < r.size()) {
-    res.push_back(r[r_i]);
-    r_i++;
-  }
-
-  return res;
-}
-
-std::vector<int> MergeSort(const std::vector<int>& arr) {
-  uint arr_size = arr.size();
-  std::vector l(arr.begin(), arr.begin() + arr_size / 2);
-  std::vector r(arr.begin() + arr_size / 2, arr.end());
-  if (l.size() <= 1 && r.size() <= 1) {
-    return Merge(l, r);
-  }
-  MergeSort(l);
-  MergeSort(r);
-  return Merge(l, r);
 }
 
 int main() {
   SetFastIo();
   Input();
-  MergeSort(a);
+
+
+  std::vector<Test> group_1;
+  std::vector<Test> group_2;
+  for (Test test : tests) {
+    test.a <= test.b ? group_1.push_back(test) : group_2.push_back(test);
+  }
+  std::sort(group_1.begin(), group_1.end(), [](Test t1, Test t2) { return t1.a < t2.b; }); // asc
+  std::sort(group_2.begin(), group_2.end(), [](Test t1, Test t2) { return t1.a > t2.b; }); // desc
+
+  std::vector<size_t> permutation;
+  permutation.reserve(n);
+  uint64_t t1 = 0;
+  uint64_t t2 = 0;
+  for (size_t i = 0; i < group_1.size(); i++) {
+    t1 += group_1[i].a;
+    t2 = std::max(t1, t2) + group_1[i].b;
+    permutation.push_back(group_1[i].id);
+  }
+  for (size_t i = 0; i < group_2.size(); i++) {
+    t1 += group_2[i].a;
+    t2 = std::max(t1, t2) + group_2[i].b;
+    permutation.push_back(group_2[i].id);
+  }
+
+  std::cout << t2 << '\n';
+  for (size_t id : permutation) {
+    std::cout << id << ' ';
+  }
+  std::cout << '\n';
+  for (size_t id : permutation) {
+    std::cout << id << ' ';
+  }
 }
